1032e601e02f8f52fdcbd9b84a3ac44542485612
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-02-18 23:44:49 -0500

    lab05 Roman Numeral

--- Lab05-RomanNumerals/Lab05-RomanNumerals/Form1.Designer.vb
+++ Lab05-RomanNumerals/Lab05-RomanNumerals/Form1.Designer.vb
@@ -22,9 +22,89 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the range 1-10" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(166, 29)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(62, 20)
+        Me.txtInput.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 52)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Equivalent Roman " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numeral" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMessage.Location = New System.Drawing.Point(166, 101)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Padding = New System.Windows.Forms.Padding(0, 0, 30, 0)
+        Me.lblMessage.Size = New System.Drawing.Size(32, 15)
+        Me.lblMessage.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(27, 188)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(132, 188)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Roman Numeral Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblMessage As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnExit As Button
 End Class
--- Lab05-RomanNumerals/Lab05-RomanNumerals/Form1.vb
+++ Lab05-RomanNumerals/Lab05-RomanNumerals/Form1.vb
@@ -1,3 +1,32 @@
 ﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Select Case CInt(txtInput.Text)
+            Case 1
+                lblMessage.Text = "I"
+            Case 2
+                lblMessage.Text = "II"
+            Case 3
+                lblMessage.Text = "III"
+            Case 4
+                lblMessage.Text = "IV"
+            Case 5
+                lblMessage.Text = "V"
+            Case 6
+                lblMessage.Text = "VI"
+            Case 7
+                lblMessage.Text = "VII"
+            Case 8
+                lblMessage.Text = "VIII"
+            Case 9
+                lblMessage.Text = "IX"
+            Case 10
+                lblMessage.Text = "X"
+            Case Else
+                lblMessage.Text = "Its not a valid value"
+        End Select
+    End Sub
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
 End Class
